8c95141ea50b6983a676e4cb3ca4c0b78b4400df
Paula Rusti <rusti.paula78@gmail.com>
2023-01-13 14:55:31 +0200

    added logic to find the winner

--- ms-game/app.js
+++ ms-game/app.js
@@ -1,4 +1,5 @@
 // computed props: -> functions returning the value of props which depend on other props
+// watchers: methods named the same as properties whose params are the old and new value of the prop being watched
 function getRandomValue() {
     return Math.floor(Math.random() * (12 - 5)) + 5;
 }
@@ -8,7 +9,8 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
     },
     methods: {
@@ -31,6 +33,16 @@ const app = Vue.createApp({
             let attackValue = getRandomValue(12, 25);
             this.monsterHealth -= attackValue;
             this.attackPlayer(); // monster strikes back
+        },
+        healPlayer() {
+            this.currentRound ++
+            let healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue <= 100) {
+                this.playerHealth += healValue;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.attackPlayer();
         }
     },
     computed: {
@@ -48,7 +60,24 @@ const app = Vue.createApp({
         }
     },
     watch: {
-
+        playerHealth(value) {
+            if (value  <= 0 && this.monsterHealth <= 0) {
+                // draw
+                this.winner = "draw"
+            } else if (value <= 0) {
+                // player lost
+                this.winner = "player"
+            }
+        },
+        monsterHealth(value) {
+            if (value  <= 0 && this.playerHealth <= 0) {
+                // draw
+                this.winner = "draw"
+            } else if (value <= 0) {
+                // monster lost
+                this.winner = "monster"
+            }
+        },
     }
 });
 
